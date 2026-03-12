858b153b147316e41ef19bbb386acc7d7c187cc5
= <=>
2026-03-12 10:50:51 +0100

    Add blur features

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -8,10 +8,12 @@ interface FeatureProps {
 
 export default function Feature({ title, description, url }: FeatureProps) {
   return (
-    <Card
-      className={`relative justify-between bg-cover bg-center bg-no-repeat p-0`}
-      style={{ backgroundImage: `url('${url}')` }}
-    >
+    <Card className="relative justify-between p-0 overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat blur-[2px] scale-105"
+        style={{ backgroundImage: `url('${url}')` }}
+      ></div>
+      <div className="absolute top-0 left-0 right-0 h-1/2 bg-linear-to-b from-black/70 to-transparent"></div>
       <h2 className="text-white text-2xl font-semibold self-center text-center z-10 p-6">
         {title}
       </h2>
